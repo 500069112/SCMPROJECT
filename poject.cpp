53a801eb5f9855a537876254ea75fe484999c3dd
500069112 <46739402+500069112@users.noreply.github.com>
2019-04-22 09:34:48 +0530

    second commit

--- poject.cpp
+++ poject.cpp
@@ -1,1 +1,24 @@
 #include<iostream.h>
+#include<cstdio>
+
+#include<cstdlib>
+
+/*
+
+ * Node Declaration
+
+ */
+
+using namespace std;
+
+struct node
+
+{
+
+    int info;
+
+    struct node *next;
+
+    struct node *prev;
+
+}*start;
