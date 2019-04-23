0dd0296b6a38bbc7717bf148b72e66287d656c89
500069625 <46739417+500069625@users.noreply.github.com>
2019-04-23 17:12:33 +0530

    add begin

--- poject.cpp
+++ poject.cpp
@@ -1,1 +1,26 @@
 #include<iostream.h>
+void circular_llist::add_begin(int value)
+
+{
+
+    if (last == NULL)
+
+    {
+
+        cout<<"First Create the list."<<endl;
+
+        return;
+
+    }
+
+    struct node *temp;
+
+    temp = new(struct node);
+
+    temp->info = value;
+
+    temp->next = last->next;
+
+    last->next = temp;
+
+}
