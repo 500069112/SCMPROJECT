cf1cbcda0bd84926536a01ca8e444250fc8566aa
500069112 <46739402+500069112@users.noreply.github.com>
2019-04-23 16:53:29 +0530

    main()

--- poject.cpp
+++ poject.cpp
@@ -53,3 +53,42 @@ class circular_llist
         }
 
 };
+int main()
+
+{
+
+    int choice, element, position;
+
+    circular_llist cl;
+
+    while (1)
+
+    {
+
+        cout<<endl<<"---------------------------"<<endl;
+
+        cout<<endl<<"Circular singly linked list"<<endl;
+
+        cout<<endl<<"---------------------------"<<endl;
+
+        cout<<"1.Create Node"<<endl;
+
+        cout<<"2.Add at beginning"<<endl;
+
+        cout<<"3.Add after"<<endl;
+
+        cout<<"4.Delete"<<endl;
+
+        cout<<"5.Search"<<endl;
+
+        cout<<"6.Display"<<endl;
+
+        cout<<"7.Update"<<endl;
+
+        cout<<"8.Sort"<<endl;
+
+        cout<<"9.Quit"<<endl;
+
+        cout<<"Enter your choice : ";
+
+        cin>>choice;
