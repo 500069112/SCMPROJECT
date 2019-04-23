3cda6a4cb289dd91af637f4e438620c6e728233c
500069112 <46739402+500069112@users.noreply.github.com>
2019-04-23 16:48:19 +0530

    third commit

--- poject.cpp
+++ poject.cpp
@@ -22,3 +22,34 @@ struct node
     struct node *prev;
 
 }*start;
+class circular_llist
+
+{
+
+    public:
+
+        void create_node(int value);
+
+        void add_begin(int value);
+
+        void add_after(int value, int position);
+
+        void delete_element(int value);
+
+        void search_element(int value);
+
+        void display_list();
+
+        void update();
+
+        void sort();
+
+        circular_llist()
+
+        {
+
+            last = NULL;
+
+        }
+
+};
